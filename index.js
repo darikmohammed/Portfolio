9673fbd9d9dca0902ec97dc2bec72b874fca2885
darikmohammed <r.m.darik@gmail.com>
2022-05-12 13:37:42 +0300

    Fix linter error

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
-const mobileMenuOpen = document.querySelector(".mobile-hamburger-btn");
-const mobileMenuClose = document.querySelector(".mobile-close-btn");
+const mobileMenuOpen = document.querySelector('.mobile-hamburger-btn');
+const mobileMenuClose = document.querySelector('.mobile-close-btn');
 const projects = [
   {
-    name: "Keeping track of hundreds of components",
-    technologies: ["Codekit", "Github", "JavaScript"],
-    featuredImage: "Portfolio1.png",
+    name: 'Keeping track of hundreds of components',
+    technologies: ['Codekit', 'Github', 'JavaScript'],
+    featuredImage: 'Portfolio1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy.",
-    liveLink: "#",
-    linkSource: "#",
+    liveLink: '#',
+    linkSource: '#',
   },
   {
-    name: "Keeping track of hundreds of components",
-    technologies: ["Codekit", "Github", "JavaScript"],
-    featuredImage: "Portfolio1.png",
+    name: 'Keeping track of hundreds of components',
+    technologies: ['Codekit', 'Github', 'JavaScript'],
+    featuredImage: 'Portfolio1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer ",
-    liveLink: "#",
-    linkSource: "#",
+    liveLink: '#',
+    linkSource: '#',
   },
   {
-    name: "Keeping track of hundreds of components",
-    technologies: ["Codekit", "Github", "JavaScript"],
-    featuredImage: "Portfolio1.png",
+    name: 'Keeping track of hundreds of components',
+    technologies: ['Codekit', 'Github', 'JavaScript'],
+    featuredImage: 'Portfolio1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    liveLink: "#",
-    linkSource: "#",
+    liveLink: '#',
+    linkSource: '#',
   },
   {
-    name: "Keeping track of hundreds of components",
-    technologies: ["Codekit", "Github", "JavaScript"],
-    featuredImage: "Portfolio1.png",
+    name: 'Keeping track of hundreds of components',
+    technologies: ['Codekit', 'Github', 'JavaScript'],
+    featuredImage: 'Portfolio1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    liveLink: "#",
-    linkSource: "#",
+    liveLink: '#',
+    linkSource: '#',
   },
   {
-    name: "Keeping track of hundreds of components",
-    technologies: ["Codekit", "Github", "JavaScript"],
-    featuredImage: "Portfolio1.png",
+    name: 'Keeping track of hundreds of components',
+    technologies: ['Codekit', 'Github', 'JavaScript'],
+    featuredImage: 'Portfolio1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    liveLink: "#",
-    linkSource: "#",
+    liveLink: '#',
+    linkSource: '#',
   },
   {
-    name: "Keeping track of hundreds of components",
-    technologies: ["Codekit", "Github", "JavaScript"],
-    featuredImage: "Portfolio1.png",
+    name: 'Keeping track of hundreds of components',
+    technologies: ['Codekit', 'Github', 'JavaScript'],
+    featuredImage: 'Portfolio1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    liveLink: "#",
-    linkSource: "#",
+    liveLink: '#',
+    linkSource: '#',
   },
 ];
 
-mobileMenuOpen.addEventListener("click", () => {
-  document.querySelector(".logo").style.display = "none";
-  document.querySelector(".mobile-menu-btn").style.display = "none";
-  document.querySelector(".desktop-nav").style.display = "flex";
+mobileMenuOpen.addEventListener('click', () => {
+  document.querySelector('.logo').style.display = 'none';
+  document.querySelector('.mobile-menu-btn').style.display = 'none';
+  document.querySelector('.desktop-nav').style.display = 'flex';
 });
 
-mobileMenuClose.addEventListener("click", () => {
-  document.querySelector(".logo").style.display = "block";
-  document.querySelector(".mobile-menu-btn").style.display = "block";
-  document.querySelector(".desktop-nav").style.display = "none";
+mobileMenuClose.addEventListener('click', () => {
+  document.querySelector('.logo').style.display = 'block';
+  document.querySelector('.mobile-menu-btn').style.display = 'block';
+  document.querySelector('.desktop-nav').style.display = 'none';
 });
 
-const menuListItems = document.querySelectorAll(".animate li");
+const menuListItems = document.querySelectorAll('.animate li');
 
 menuListItems.forEach((link) => {
-  link.addEventListener("click", () => {
-    document.querySelector(".logo").style.display = "block";
-    document.querySelector(".mobile-menu-btn").style.display = "block";
-    document.querySelector(".desktop-nav").style.display = "none";
+  link.addEventListener('click', () => {
+    document.querySelector('.logo').style.display = 'block';
+    document.querySelector('.mobile-menu-btn').style.display = 'block';
+    document.querySelector('.desktop-nav').style.display = 'none';
   });
 });
 
@@ -84,11 +84,11 @@ menuListItems.forEach((link) => {
 
 function generateWorkCard() {
   projects.forEach((project, index) => {
-    let technology = "";
+    let technology = '';
     project.technologies.forEach((tech) => {
       technology += `<li>${tech}</li>`;
     });
-    document.querySelector(".work-cards").innerHTML += `
+    document.querySelector('.work-cards').innerHTML += `
     <div class="card">
       <div class="project-img">
         <img src="./icons/Projects/${project.featuredImage}" alt="project card" />
@@ -108,69 +108,66 @@ function generateWorkCard() {
 generateWorkCard();
 
 // Popup modals.
-const popupModals = document.querySelectorAll(".popup-window");
-const closeModal = document.querySelector(".close-modal-btn");
+const popupModals = document.querySelectorAll('.popup-window');
+const closeModal = document.querySelector('.close-modal-btn');
 function openModal(element) {
   const projectIndex = element.dataset.index;
-  document.querySelector(".modal-header").textContent =
-    projects[projectIndex].name;
+  document.querySelector('.modal-header').textContent = projects[projectIndex].name;
   document
-    .querySelector(".modal-project-image")
+    .querySelector('.modal-project-image')
     .setAttribute(
-      "src",
-      `./icons/Projects/${projects[projectIndex].featuredImage}`
+      'src',
+      `./icons/Projects/${projects[projectIndex].featuredImage}`,
     );
   document
-    .querySelector(".modal-project-image")
-    .setAttribute("alt", `${projects[projectIndex].name}`);
+    .querySelector('.modal-project-image')
+    .setAttribute('alt', `${projects[projectIndex].name}`);
   document
-    .querySelector(".moblie-see-live-btn")
-    .setAttribute("href", `${projects[projectIndex].liveLink}`);
+    .querySelector('.moblie-see-live-btn')
+    .setAttribute('href', `${projects[projectIndex].liveLink}`);
   document
-    .querySelector(".moblie-see-source-btn")
-    .setAttribute("href", `${projects[projectIndex].linkSource}`);
+    .querySelector('.moblie-see-source-btn')
+    .setAttribute('href', `${projects[projectIndex].linkSource}`);
   document
-    .querySelector(".desktop-see-live-btn")
-    .setAttribute("href", `${projects[projectIndex].liveLink}`);
+    .querySelector('.desktop-see-live-btn')
+    .setAttribute('href', `${projects[projectIndex].liveLink}`);
   document
-    .querySelector(".desktop-see-source-btn")
-    .setAttribute("href", `${projects[projectIndex].linkSource}`);
-  let technology = "";
+    .querySelector('.desktop-see-source-btn')
+    .setAttribute('href', `${projects[projectIndex].linkSource}`);
+  let technology = '';
   projects[projectIndex].technologies.forEach((tech) => {
     technology += `<div class="tag">${tech}</div>`;
   });
-  document.querySelector(".project-codekit .tags").innerHTML = technology;
-  document.querySelector(".modal-project-description").textContent =
-    projects[projectIndex].description;
-  document.querySelector(".modal").style.display = "flex";
+  document.querySelector('.project-codekit .tags').innerHTML = technology;
+  document.querySelector('.modal-project-description').textContent = projects[projectIndex].description;
+  document.querySelector('.modal').style.display = 'flex';
 }
 
 popupModals.forEach((modal) => [
-  modal.addEventListener("click", () => {
+  modal.addEventListener('click', () => {
     openModal(modal);
   }),
 ]);
-closeModal.addEventListener("click", () => {
-  document.querySelector(".modal").style.display = "none";
+closeModal.addEventListener('click', () => {
+  document.querySelector('.modal').style.display = 'none';
 });
 
-//email validate
-const form = document.querySelector("#contact-form");
-const errorEmail = document.querySelector("#email_error");
+// email validate
+const form = document.querySelector('#contact-form');
+const errorEmail = document.querySelector('#email_error');
 function validateEmail(input) {
-  let emailRegex =
-    /^[a-z_\-0-9\.\*\#\$\!\~\%\^\&\-\+\?\|]+@+[a-z\-0-9]+(.com)$/gm;
+  const emailRegex = /^[a-z_\-0-9.*#$!~%^&-+?|]+@+[a-z\-0-9]+(.com)$/gm;
   return emailRegex.test(input);
 }
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  let emailValid = validateEmail(form.elements["email"].value);
-  //if valid email
+  const emailValid = validateEmail(form.elements.email.value);
+  // if valid email
   if (!emailValid) {
-    //send error
-    errorEmail.style.display = "block";
+    // send error
+    errorEmail.style.display = 'block';
   } else {
-    errorEmail.style.display = "none";
+    errorEmail.style.display = 'none';
     form.submit();
   }
 });
